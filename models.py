6f73ef22d0720ead7b5b7deeaf66b09374e37c4b
Shawaiz Asghar <112537364+shawaizasghar786@users.noreply.github.com>
2025-09-27 00:54:45 +0500

    pretrained model calls

---
+++ models.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+class EncoderCNN(nn.Module):
+    def __init__(self, embed_size):
+        super().__init__()
+        resnet=models.resnet50(pretrained=True)
+        modules=list(resnet.children())[:-1]
+        self.resnet=nn.Sequential(*modules)
+        self.linear=nn.Linear(resnet.fc.in_feature,embed_size)
+        self.bn=nn.BatchNorm1d(embed_size)
+    

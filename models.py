518df51ae46b6b6e8dad42bd006753d66f3d5a7f
Shawaiz Asghar <112537364+shawaizasghar786@users.noreply.github.com>
2025-09-27 01:01:32 +0500

    Encoder+Decorder implemented

--- models.py
+++ models.py
@@ -14,4 +14,17 @@ class EncoderCNN(nn.Module):
         with torch.no_grad():
             features=self.resnet(image).squeeze()
             return self.bn(self.linear(features))
-        
+
+    class DecoderRNN(nn.Module):
+      def __init__(self, embed_size, hidden_size, vocab_size, num_layers):
+        super().__init__()
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, vocab_size)   
+
+    def forward(self,features,captions):
+       embeddings=self.embed(captions)
+       inputs=torch.cat((features.unsqueeze(1),embeddings),1)
+       hiddens,_=self.lstm(inputs)
+       outputs=self.linear(hiddens)
+       return outputs

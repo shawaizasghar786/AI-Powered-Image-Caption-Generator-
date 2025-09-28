d60c5667521c172fdb9494db1292e384bc265f4c
Shawaiz Asghar <112537364+shawaizasghar786@users.noreply.github.com>
2025-09-28 13:03:41 +0500

    update

--- train.py
+++ train.py
@@ -1,11 +1,15 @@
 import torch
 from torch.utils.data import DataLoader
 from torch import nn, optim
-from models import EncoderCNN, DecoderRNN
+from models import EncoderCNN, DecoderWithAttention
 from utils.dataset import FlickrDataset
 from config import *
+import json
+from tqdm import tqdm
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Load dataset and vocab
 dataset = FlickrDataset("data/Flickr8k_Dataset", "data/Flickr8k_text/Flickr8k.token.txt", "utils/vocab.json")
 dataloader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True, collate_fn=lambda x: zip(*x))
 
@@ -13,21 +17,33 @@ with open("utils/vocab.json") as f:
     vocab = json.load(f)
 vocab_size = len(vocab)
 
-encoder = EncoderCNN(EMBED_SIZE).to(device)
-decoder = DecoderRNN(EMBED_SIZE, HIDDEN_SIZE, vocab_size, NUM_LAYERS).to(device)
+# Initialize models
+encoder = EncoderCNN().to(device)
+decoder = DecoderWithAttention(EMBED_SIZE, HIDDEN_SIZE, vocab_size).to(device)
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(list(encoder.parameters()) + list(decoder.parameters()), lr=LEARNING_RATE)
+criterion = nn.CrossEntropyLoss(ignore_index=vocab['<PAD>'])
+optimizer = optim.Adam(list(decoder.parameters()) + list(encoder.parameters()), lr=LEARNING_RATE)
 
+# Training loop
 for epoch in range(NUM_EPOCHS):
-    for images, captions in dataloader:
+    encoder.train()
+    decoder.train()
+    total_loss = 0
+
+    for images, captions in tqdm(dataloader, desc=f"Epoch {epoch+1}"):
         images = torch.stack(images).to(device)
         captions = torch.nn.utils.rnn.pad_sequence(captions, batch_first=True, padding_value=vocab['<PAD>']).to(device)
-        outputs = decoder(encoder(images), captions[:, :-1])
+
+        encoder_out = encoder(images)
+        outputs = decoder(encoder_out, captions[:, :-1])
         loss = criterion(outputs.reshape(-1, vocab_size), captions[:, 1:].reshape(-1))
+
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-    print(f"Epoch {epoch+1}, Loss: {loss.item():.4f}")
+
+        total_loss += loss.item()
+
+    print(f"Epoch {epoch+1} Loss: {total_loss:.4f}")
     torch.save(encoder.state_dict(), f"models/encoder_epoch{epoch+1}.pth")
     torch.save(decoder.state_dict(), f"models/decoder_epoch{epoch+1}.pth")
